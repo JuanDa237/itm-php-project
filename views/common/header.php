24e7b0fad74e8528b37a649726bb562726e75471
JuanDa237 <juandavid200301@hotmail.com>
2022-08-20 12:34:59 -0500

    first commit

---
+++ views/common/header.php
@@ -0,0 +1,20 @@
+<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between p-3 mb-4 border-bottom">
+	<a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+		<img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Logo_TV_2015.png" alt="Company logo" width="30" height="24" class="d-inline-block align-text-top">
+		Company Name
+	</a>
+
+	<ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+		<li>
+			<a href="#" class="nav-link px-2 link-secondary">Home</a>
+		</li>
+		<li>
+			<a href="#" class="nav-link px-2 link-dark">Products</a>
+		</li>
+	</ul>
+
+	<div class="col-md-3 text-end">
+		<a class="btn btn-outline-primary me-2">Login</a>
+		<a class="btn btn-primary">Sign-up</a>
+	</div>
+</header>
