1bd1575e7346711b171cfbc3f14013bd1cfd516d
JuanDa237 <juandavid200301@hotmail.com>
2022-08-20 13:10:03 -0500

    - Change folder structure

---
+++ views/home.php
@@ -0,0 +1,19 @@
+<?php
+// start.php vars
+$doc_title = "Company Name | Home";
+?>
+
+<?php require("./views/layout/start.php") ?>
+<?php require("./views/components/common/header.php") ?>
+
+<!-- Styles -->
+<?php require("./views/styles/index-css.php") ?>
+
+<!-- Content -->
+<div class="container flex-grow-1 marketing">
+	<?php require("./views/components/home/members.php") ?>
+	<?php require("./views/components/home/company.php") ?>
+</div>
+
+<?php require("./views/components/common/footer.php") ?>
+<?php require("./views/layout/end.php") ?>
